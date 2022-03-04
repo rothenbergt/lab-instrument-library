d72c2483468dbad742526dc1a80ef16f58c5656d
Tyler Rothenberg <rothenbergt@gmail.com>
2022-03-04 13:32:43 -0500

    Update Multimeter.py
    Update library functionality

--- Multimeter.py
+++ Multimeter.py
@@ -29,6 +29,11 @@ The current methods available within the module are:
   mult = Multimeter("GPIB0::15::INSTR")
   measured_voltage = mult.measure_voltage()
 
+  OR
+
+  mult = Multimeter("GPIB0::15::INSTR", identify = False, nickname = "Keithly 2000")
+  measured_voltage = mult.measure_voltage()
+  
   --------------------------------------------------------------------------------------------------- |
   | COMPANY     MODEL   DOCUMENT      LINK                                                            |
   --------------------------------------------------------------------------------------------------- |
@@ -50,6 +55,10 @@ class Multimeter():
 
     Attributes:
         lab_multimeters: A dictionary of the current lab multimeters.
+        instrument_address:
+        connection:
+        instrument_ID:
+        nickname:
     """
     lab_multimeters = {
         "1": "2000",
@@ -58,14 +67,14 @@ class Multimeter():
         "4": "34401A",
     }
     
-    def __init__(self, instrument_address = "GPIB0::20::INSTR", nickname = None, identify = True):
-        '''
-        Disables the output of the power supply
-        '''
-        """Gets the selected function.
+    def __init__(self, instrument_address: str, nickname: str = None, identify: bool = True):
+        """Initalizes the instance with necessary information.
 
         Args:
-        minimum: The selected channel
+        instrument_address: The selected channel
+        nickname: string which allows naming of a device yourself. 
+        identify: boolean which will either identify the instrument with *IDN? or not.
+                  This is useful as some instruments do not have this command built in.
 
         Returns:
         The current selected function.
@@ -78,18 +87,21 @@ class Multimeter():
         self.connection = None
         self.instrument_ID = None
         self.nickname = nickname
-        if (not self.make_connection(instrument_address, identify)):
+
+        if (not self.make_connection(identify)):
             sys.exit()
 
     # Make the GPIB connection & set up the instrument
-    def make_connection(self, instrument_address, identify) -> bool:
-        """Gets the selected function.
+    def make_connection(self, identify: bool) -> bool:
+        """Attempts to make a GPIB PyVISA connection with instrument .
 
         Args:
-        minimum: The selected channel
+        instrument_address: The selected channel
+        identify: boolean which will either identify the instrument with *IDN? or not.
+                  This is useful as some instruments do not have this command built in. 
 
         Returns:
-        The current selected function.
+        boolean for if connection was made successfully
 
         Raises:
         Except: If the query fails.
@@ -97,41 +109,49 @@ class Multimeter():
         # Make the connection
         try:
             # Make the connection and store it
-            self.connection = self.rm.open_resource(instrument_address)
-            
-            # Increase the timeout
-            # self.connection.timeout = 5000          
+            self.connection = self.rm.open_resource(self.instrument_address)
             
             # Display that the connection has been made
             if identify == True:              
-                self.identify()
+                if (self.identify()):
+                    print(f"Successfully established {self.instrument_address} connection with {self.instrument_ID}")
+                    return True
+                else:
+                    return False
+            else:
+                if self.nickname == None:
+                    print("If identify is false nickname must be set")
+                    sys.exit()
+                else:
+                    self.instrument_ID = self.nickname
+
                 print(f"Successfully established {self.instrument_address} connection with {self.instrument_ID}")
                 return True
-            else:
-                print(f"Successfully established {self.instrument_address} connection with {self.nickname}")
-                return True
         
-        except:
-            print(f"Failed to establish {self.instrument_address} connection.")
+        except Exception as ex:
+            print(f"General exception connecting to {self.instrument_address} connection.")
+            print(ex)
             return False
             
 
-    def identify(self):
-        """Gets the selected function.
+    def identify(self) -> bool:
+        """Identifies the instrument ID using *IDN? query.
 
         Args:
-        minimum: The selected channel
+        none
 
         Returns:
-        The current selected function.
+        bool if the identification was successful or not
 
         Raises:
-        Except: If the query fails.
+        Except: If the identification fails.
         """
         try:
             self.instrument_ID = self.connection.query("*IDN?")[:-1]
+            return True
         except:
-            print("Unit could not by identified using *IDN? command")
+            print(f"{self.__class__.__name__} at {self.instrument_address} could not by identified using *IDN?")
+            return False
 
 
     def initiate(self) -> bool:
@@ -171,10 +191,10 @@ class Multimeter():
                 print(f"Device {self.instrument_ID} not in library")
         
         except ValueError as ex:
-            # print(f"Could not convert returned value from meter: {self.instrumentID} at {self.instrument}")
+            # print(f"Could not convert returned value from meter: {self.instrument_ID} at {self.instrument}")
             return retval
         except Exception as ex:
-            # print(f"Could not fetch from meter: {self.instrumentID} at {self.instrument}")
+            # print(f"Could not fetch from meter: {self.instrument_ID} at {self.instrument}")
             return retval
 
     def fetch_voltage(self) -> float:
@@ -190,39 +210,49 @@ class Multimeter():
 
         Raises:
         ValueError: If the result from the multimeter couldn't be convereted to float.
-        Exception: GPIB Error retreiving from multimeter
+        pyvisa.errors.VisaIOError: 
+        pyvisa.errors.VisaIOErrorVI_ERROR_NLISTENERS:
+        pyvisa.errors.VI_ERROR_TMO
         """
         retval = sys.maxsize 
         
         try:
-            if "2000" in self.instrumentID:
+            if "2000" in self.instrument_ID:
                 retval = float(self.connection.query("FETCh?"))
                 return retval
 
-            elif "2110" in self.instrumentID:
+            elif "2110" in self.instrument_ID:
                 retval = float(self.connection.query("FETCh?"))
                 return retval
 
-            elif "4050" in self.instrumentID:
+            elif "4050" in self.instrument_ID:
                 retval = float(self.connection.query("FETCh?"))
                 return retval
             elif "34401A":
                 retval = float(self.connection.query("FETCh?"))
                 return retval
             else:
-                print(f"Device {self.instrumentID} not in library")
+                print(f"Device {self.instrument_ID} not in library")
                 return retval
         
         except ValueError as ex:
-            print(f"Could not convert returned value to floatfrom meter: {self.instrumentID} at {self.instrument} \n \
-                    in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}: {message}")
+            print(f"Could not convert returned value to float from meter: {self.instrument_ID} at {self.instrument} \n \
+                    in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}")
             return retval
+
         except pyvisa.errors.VisaIOError as ex:
-            print(f"Input/Output error. Check your command in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}: {message}")
-            return retval
+            
+            if "VI_ERROR_NLISTENERS" in ex.abbreviation:
+                print(f"Looks like the instrument at {self.instrument_address} isn't responding.. Are you sure this is the right GPIB address?.")
+                return retval
+            else:
+                print(f"Input/Output error. Check your command in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}")
+                return retval
+
         except pyvisa.errors.VI_ERROR_TMO as ex:
-            print(f"Device {self.instrumentID} timeout error in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}")
+            print(f"Device {self.instrument_ID} timeout error in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}")
             return retval
+    
 
 
     def read_voltage(self):
@@ -241,9 +271,9 @@ class Multimeter():
         try:
             retval = float(self.connection.query("READ?"))
         except ValueError as ex:
-            print(f"Could not convert returned value from meter: {self.instrumentID} at {self.instrument}")
+            print(f"Could not convert returned value from meter: {self.instrument_ID} at {self.instrument}")
         except Exception as ex:
-            print(f"Could not fetch from meter: {self.instrumentID} at {self.instrument}")
+            print(f"Could not fetch from meter: {self.instrument_ID} at {self.instrument}")
         return retval
 
 
@@ -264,11 +294,11 @@ class Multimeter():
         try:
             retval = float(self.connection.query(f"MEASure:{function}?"))
         except ValueError as ex:
-            print(f"Could not convert returned value from meter: {self.instrumentID} at {self.instrument}")
+            print(f"Could not convert returned value from meter: {self.instrument_ID} at {self.instrument}")
         except TimeoutError as ex:
-            print(f"Timeout error from meter: {self.instrumentID} at {self.instrument}")
+            print(f"Timeout error from meter: {self.instrument_ID} at {self.instrument}")
         except Exception as ex:
-            print(f"Could not fetch from meter: {self.instrumentID} at {self.instrument}")
+            print(f"Could not fetch from meter: {self.instrument_ID} at {self.instrument}")
         return retval
 
     def get_function(self):
@@ -287,14 +317,28 @@ class Multimeter():
             current_function = self.connection.query("FUNCtion?")
             return current_function
         except:
-            print(f"General Exception from meter: {self.instrumentID} at {self.instrument}")
+            print(f"General Exception from meter: {self.instrument_ID} at {self.instrument}")
 
     def set_function(self, function: str):
         """Gets the selected function.
 
         Args:
-        minimum: The selected channel
-
+        function: The selected channel
+                                            VOLTage:AC"
+                                            "VOLTage[:DC]"
+                                            "VOLTage[:DC]:RATio"
+                                            "CURRent:AC"
+                                            "CURRent[:DC]"
+                                            "FREQuency[:VOLT]"
+                                            "FREQuency:CURR"
+                                            "FRESistance"
+                                            "PERiod[:VOLT]"
+                                            "PERiod:CURR"
+                                            "RESistance"
+                                            "DIODe"
+                                            "TCOuple"
+                                            "TEMPerature"
+                                            "CONTinuity"
         Returns:
         The current selected function.
 
@@ -302,26 +346,15 @@ class Multimeter():
         Except: If the query fails.
         """
         '''
-        VOLTage:AC"
-        "VOLTage[:DC]"
-        "VOLTage[:DC]:RATio"
-        "CURRent:AC"
-        "CURRent[:DC]"
-        "FREQuency[:VOLT]"
-        "FREQuency:CURR"
-        "FRESistance"
-        "PERiod[:VOLT]"
-        "PERiod:CURR"
-        "RESistance"
-        "DIODe"
-        "TCOuple"
-        "TEMPerature"
-        "CONTinuity"
-        Note: DIODe and
+
         '''
         self.connection.write(f":conf:{function}")
 
         return self.get_function()
+
+    def set_thermocouple_type(self, thermocouple_type: str):
+        self.connection.write(f"TCOuple:TYPE {thermocouple_type}")
+
 
     def get_voltage_range():
         """Gets the selected function.
@@ -352,32 +385,32 @@ class Multimeter():
         retval = sys.maxsize 
         
         try:
-            if "2000" in self.instrumentID:
+            if "2000" in self.instrument_ID:
 
                 # Path to configure measurement range:
                 # Select range (0 to 1010).
                 retval = self.connection.query(f"{function}:RANGe?")
                 return float(retval)
 
-            elif "2110" in self.instrumentID:
+            elif "2110" in self.instrument_ID:
                 retval = self.connection.query(f"{function}:RANGe?")
                 return float(retval)
 
-            elif "4050" in self.instrumentID:
+            elif "4050" in self.instrument_ID:
                 retval = self.connection.query(f"{function}:RANGe?")
                 return float(retval)
             elif "34401A":
                 retval = self.connection.query(f"{function}:RANGe?")
                 return float(retval)
             else:
-                print(f"Device {self.instrumentID} not in library")
+                print(f"Device {self.instrument_ID} not in library")
                 return retval
         
         except ValueError as ex:
-            # print(f"Could not convert returned value from meter: {self.instrumentID} at {self.instrument}")
+            # print(f"Could not convert returned value from meter: {self.instrument_ID} at {self.instrument}")
             return retval
         except Exception as ex:
-            # print(f"Could not fetch from meter: {self.instrumentID} at {self.instrument}")
+            # print(f"Could not fetch from meter: {self.instrument_ID} at {self.instrument}")
             return retval      
 
     def set_voltage_range(self, voltage_range):
@@ -410,30 +443,30 @@ class Multimeter():
         retval = sys.maxsize 
         
         try:
-            if "2000" in self.instrumentID:
+            if "2000" in self.instrument_ID:
 
                 # Path to configure measurement range:
                 # Select range (0 to 1010).
                 self.connection.write(f"{function}:RANGe {voltage_range}")
 
-            elif "2110" in self.instrumentID:
+            elif "2110" in self.instrument_ID:
                 self.connection.write(f"{function}:RANGe {voltage_range}")
 
-            elif "4050" in self.instrumentID:
+            elif "4050" in self.instrument_ID:
                 self.connection.write(f"{function}:RANGe {voltage_range}")
 
             elif "34401A":
                 self.connection.write(f"{function}:RANGe {voltage_range}")
 
             else:
-                print(f"Device {self.instrumentID} not in library")
+                print(f"Device {self.instrument_ID} not in library")
                 return retval
         
         except ValueError as ex:
-            # print(f"Could not convert returned value from meter: {self.instrumentID} at {self.instrument}")
+            # print(f"Could not convert returned value from meter: {self.instrument_ID} at {self.instrument}")
             return retval
         except Exception as ex:
-            # print(f"Could not fetch from meter: {self.instrumentID} at {self.instrument}")
+            # print(f"Could not fetch from meter: {self.instrument_ID} at {self.instrument}")
             return retval
 
 
@@ -459,9 +492,9 @@ class Multimeter():
             return error_string
             
         except ValueError as ex:
-            print(f"Could not convert returned value from meter: {self.instrumentID} at {self.instrument}")        
+            print(f"Could not convert returned value from meter: {self.instrument_ID} at {self.instrument}")        
         except Exception as ex:
-            print(f"General Exception from meter: {self.instrumentID} at {self.instrument}")
+            print(f"General Exception from meter: {self.instrument_ID} at {self.instrument}")
 
     def query(self, message):
         try:
@@ -469,16 +502,20 @@ class Multimeter():
         except pyvisa.errors.VisaIOError as ex:
             print(f"Input/Output error. Check your command in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}: {message}")
         except pyvisa.errors.VI_ERROR_TMO as ex:
-            print(f"Device {self.instrumentID} timeout error in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}")
+            print(f"Device {self.instrument_ID} timeout error in class {self.__class__.__name__}, method {inspect.currentframe().f_code.co_name}")
 
 # TODO :init:cont off for 2000
 
-mult_2000 = Multimeter("GPIB0::3::INSTR")
-# mult_2110 = Multimeter("USB0::0x05E6::0x2110::8015791::INSTR")
+
+
+# mult_2000 = Multimeter("GPIB0::3::INSTR", identify = False, nickname = "2000")
+# print(mult_2000.fetch_voltage())
+# mult_2000 = Multimeter("GPIB0::13::INSTR", identify = False, nickname = "2000")
+# print(mult_2000.fetch_voltage())
+mult_2110 = Multimeter("USB0::0x05E6::0x2110::8015791::INSTR")
 # mult_4050 = Multimeter("GPIB0::2::INSTR")
 # mult_34401A = Multimeter("GPIB0::1::INSTR")
 
-mult_2000.query("testing")
 
 # # mult_2000.initiate()
 # # mult_2110.initiate()
@@ -491,9 +528,15 @@ mult_2000.query("testing")
 # # print(mult_34401A.fetch_voltage())
 
 # print(mult_2000.set_function('volt:dc'))
-# print(mult_2110.set_function('volt:dc'))
+# print(mult_2000.set_function('TCOuple'))
+
+print(mult_2110.set_function('TCOuple'))
+mult_2110.set_thermocouple_type("T")
 # print(mult_4050.set_function('volt:dc'))
 # print(mult_34401A.set_function('volt:dc'))
+
+mult_2110.initiate()
+print(mult_2110.fetch_voltage())
 
 # print(mult_2000.set_voltage_range(2))
 # print(mult_2110.set_voltage_range(2))
